eeba51043f104d6ecc5f47be20ac50379bb77c5c
Furkan Bulut <furkanbtng@gmail.com>
2024-07-02 19:06:26 +0300

    weather api integrated a little bit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:weather/weather.dart';
+import 'package:intl/intl.dart';
+import 'package:map_tracker/utils/constants.dart';
 import 'package:map_tracker/services/auth_service.dart';
 import 'profile_screen.dart';
-import 'package:map_tracker/services/weather_service.dart';
-import 'package:map_tracker/models/weather_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,22 +14,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  List<WeatherModel> _weathers = [];
-
-  void _getWeatherData() async {
-    _weathers = await WeatherService().getWeatherData();
-    setState(() {});
-  }
+  final WeatherFactory _wf = WeatherFactory(OPENWEATHER_API_KEY);
+  Weather? _weather;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  int _selectedIndex = 0;
 
   @override
   void initState() {
-    _getWeatherData();
     super.initState();
+    _fetchWeather();
   }
 
-  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-  int _selectedIndex = 0;
+  void _fetchWeather() async {
+    Weather? weather = await _wf.currentWeatherByCityName("Istanbul");
+    setState(() {
+      _weather = weather;
+    });
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -40,59 +42,11 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final User? user = ModalRoute.of(context)?.settings.arguments as User?;
 
-    List<Widget> _widgetOptions = <Widget>[
-      // Ana içerik
-      Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            if (user != null)
-              Column(
-                children: [
-                  ListTile(
-                    title: Text("Kullanıcı Adı: ${user.displayName ?? 'Bilinmiyor'}"),
-                    subtitle: Text("Email: ${user.email}"),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(user.photoURL ?? ""),
-                    ),
-                  ),
-                  const SizedBox(height: 16.0),
-                ],
-              ),
-            if (user == null)
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text("Giriş Yapılmadı"),
-              ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/new_activity');
-              },
-              child: Text("Yeni Aktivite"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/activity_history');
-              },
-              child: Text("Aktivite Geçmişim"),
-            ),
-          ],
-        ),
-      ),
-      // Profil ekranı
-      if (user != null) ProfilePage(user: user) else const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text("Giriş Yapılmadı"),
-      ),
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: Text("Map Tracker App"),
         centerTitle: true,
-        automaticallyImplyLeading: false, // Geri butonunu göster
-
+        automaticallyImplyLeading: false,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.exit_to_app),
@@ -103,9 +57,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body:
-
-      _widgetOptions.elementAt(_selectedIndex),
+      body: _buildBody(user),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -120,6 +72,102 @@ class _HomePageState extends State<HomePage> {
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  Widget _buildBody(User? user) {
+    if (_weather == null) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _buildUserInfo(user),
+        Expanded(
+          child: _selectedIndex == 0 ? _buildHomeScreen(user) : _buildSelectedScreen(user),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildUserInfo(User? user) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (user != null)
+            Column(
+              children: [
+                ListTile(
+                  title: Text("Kullanıcı Adı: ${user.displayName ?? 'Bilinmiyor'}"),
+                  subtitle: Text("Email: ${user.email}"),
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(user.photoURL ?? ""),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+              ],
+            ),
+          if (user == null)
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text("Giriş Yapılmadı"),
+            ),
+          _locationHeader(),
+          const SizedBox(height: 8.0),
+          _dateTimeInfo(),
+          const SizedBox(height: 16.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildHomeScreen(User? user) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        // Add any additional widgets for the home screen here
+      ],
+    );
+  }
+
+  Widget _buildSelectedScreen(User? user) {
+    if (user != null) {
+      switch (_selectedIndex) {
+        case 1:
+          return ProfilePage(user: user);
+        default:
+          return Container(); // Handle other cases as needed
+      }
+    } else {
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text("Giriş Yapılmadı"),
+      );
+    }
+  }
+
+  Widget _locationHeader() {
+    return Text(
+      _weather!.areaName ?? "Bilinmiyor",
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _dateTimeInfo() {
+    DateTime now = _weather!.date!;
+    return Text(
+      DateFormat("h:mm a").format(now),
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
       ),
     );
   }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,3 +4,6 @@ import 'package:flutter/material.dart';
 const kBackgroundColor = Color(0xFFD2FFF4);
 const kPrimaryColor = Color(0xFF2D5D70);
 const kSecondaryColor = Color(0xFF265DAB);
+
+
+const OPENWEATHER_API_KEY = 'd6143ca52fdfb839fb639b667daea1f0';
