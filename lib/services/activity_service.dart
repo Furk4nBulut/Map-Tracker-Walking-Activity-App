2ddc848660cdf7a4df60b108e578a0f47365695c
Furkan Bulut <furkanbtng@gmail.com>
2024-07-16 17:18:47 +0300

    firebase added again  activity service

--- lib/services/activity_service.dart
+++ lib/services/activity_service.dart
@@ -2,9 +2,12 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:map_tracker/services/local_db_service.dart';
 import 'package:map_tracker/model/activity_model.dart';
 import 'package:map_tracker/model/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ActivityService {
   final DatabaseHelper _dbHelper = DatabaseHelper();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> saveActivity({
     required LocalUser user,
@@ -32,7 +35,30 @@ class ActivityService {
         endPositionLng: endPosition?.longitude,
         route: route,
       ));
-    } catch (e) {
+
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+      User? firebaseuser = _auth.currentUser;
+      if (firebaseuser != null) {
+
+      // Save activity to Firestore
+      await _firestore.collection('user').doc(firebaseuser.uid).collection('activities').add({
+        'userId': user.id,
+        'startTime': startTime,
+        'endTime': endTime,
+        'totalDistance': totalDistance,
+        'elapsedTime': elapsedTime,
+        'averageSpeed': averageSpeed,
+        'startPosition': startPosition != null
+            ? {'latitude': startPosition.latitude, 'longitude': startPosition.longitude}
+            : null,
+        'endPosition': endPosition != null
+            ? {'latitude': endPosition.latitude, 'longitude': endPosition.longitude}
+            : null,
+        'route': route.map((latLng) => {'latitude': latLng.latitude, 'longitude': latLng.longitude}).toList(),
+      });
+    }
+    }
+      catch (e) {
       print('An error occurred while saving the activity: $e');
       throw 'An error occurred while saving the activity: $e';
     }
