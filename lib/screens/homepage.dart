b92030ef8a3698f1fa0786869aa318f9b6e31c52
FurkanBlt <furkanbtng@gmail.com>
2024-07-21 19:26:16 +0300

    homepage information added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -99,18 +99,143 @@ class _HomePageState extends State<HomePage> {
         ? "${localUser!.firstName} ${localUser!.lastName}"
         : firebaseUser?.displayName ?? "Misafir";
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        _buildWeatherWidget(),
-        _buildUserInfo(),
-        Expanded(
-          child: Center(
-            child: Text("Hoşgeldin! $displayName"),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _buildWeatherWidget(),
+          _buildUserInfo(),
+          _buildSectionDivider(),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: RichText(
+              textAlign: TextAlign.justify,
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Bu uygulama, yürüyüş ve koşu aktivitelerinizi daha etkili bir şekilde yönetmenize yardımcı olur. İşte uygulamanın sunduğu bazı özellikler:",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "\n\n1. ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: Icon(Icons.location_on, color: Colors.blue[800], size: 20),
+                  ),
+                  TextSpan(
+                    text: " Yürüyüş ve koşu aktivitelerinizi takip eder: ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[800],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Uygulama, başladığınız her yeni yürüyüş veya koşu aktivitesinde mevcut konumunuzu GPS üzerinden takip eder ve harita üzerinde rotanızı çizer.",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "\n\n2. ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: Icon(Icons.access_time, color: Colors.green[800], size: 20),
+                  ),
+                  TextSpan(
+                    text: " Anlık veri görüntüleme: ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[800],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Aktivite sırasında kaydedilen mesafe, süre ve ortalama hız gibi bilgileri anlık olarak görüntüleyebilirsiniz.",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "\n\n3. ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: Icon(Icons.storage, color: Colors.orange[800], size: 20),
+                  ),
+                  TextSpan(
+                    text: " Veri kaydetme ve analiz: ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[800],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Aktivitenizi tamamladığınızda, bu bilgiler uygulama içindeki veritabanına kaydedilir. Daha sonra bu verileri gözden geçirebilir, analiz edebilir ve geçmiş aktivitelerinizi detaylı bir şekilde inceleyebilirsiniz.",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "\n\n4. ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: Icon(Icons.wb_sunny, color: Colors.yellow[800], size: 20),
+                  ),
+                  TextSpan(
+                    text: " Hava durumu bilgileri: ",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[800],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Uygulama, hava durumu bilgilerini sunarak hava koşullarına göre planlama yapmanıza olanak tanır.",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                  TextSpan(
+                    text: "\n\nUygulama sayesinde, hem mevcut hem de geçmiş aktivitelerinizle ilgili kapsamlı bilgiler edinerek spor alışkanlıklarınızı daha etkin bir şekilde yönetebilirsiniz.",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey[800],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
-      ],
+          _buildSectionDivider(),
+        ],
+      ),
     );
+
   }
 
   Widget _buildUserInfo() {
@@ -121,15 +246,33 @@ class _HomePageState extends State<HomePage> {
         children: [
           if (localUser != null)
             ListTile(
-              title: Text("Email: ${localUser!.email}"),
+              title: Text(
+                "Email: ${localUser!.email}",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black87,
+                ),
+              ),
               leading: CircleAvatar(
                 child: const Icon(Icons.person),
               ),
             )
           else if (firebaseUser != null)
             ListTile(
-              title: Text("Adı Soyad: ${firebaseUser!.displayName}"),
-              subtitle: Text("Email: ${firebaseUser!.email}"),
+              title: Text(
+                "Adı Soyad: ${firebaseUser!.displayName}",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black87,
+                ),
+              ),
+              subtitle: Text(
+                "Email: ${firebaseUser!.email}",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[600],
+                ),
+              ),
               leading: CircleAvatar(
                 backgroundImage: NetworkImage(firebaseUser!.photoURL ?? ''),
               ),
@@ -137,7 +280,13 @@ class _HomePageState extends State<HomePage> {
           else
             const Padding(
               padding: EdgeInsets.all(16.0),
-              child: Text("Giriş Yapılmadı"),
+              child: Text(
+                "Giriş Yapılmadı",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                ),
+              ),
             ),
         ],
       ),
@@ -148,6 +297,15 @@ class _HomePageState extends State<HomePage> {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 0.0),
       child: WeatherWidget(),
+    );
+  }
+
+  Widget _buildSectionDivider() {
+    return Divider(
+      color: Colors.blue[800],
+      thickness: 1,
+      indent: 16,
+      endIndent: 16,
     );
   }
 }
