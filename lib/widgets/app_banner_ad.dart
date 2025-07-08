b58d73989fa46161c26a2cd80624728c16292d66
Furk4nBulut <furkanbtng@gmail.com>
2025-07-08 14:39:16 +0300

    New actiticty screen bug fixed.

--- lib/widgets/app_banner_ad.dart
+++ lib/widgets/app_banner_ad.dart
@@ -2,13 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:map_tracker/widgets/admob_banner.dart';
 import 'package:map_tracker/services/ad_service.dart';
 
-class AppBannerAd extends StatelessWidget {
+class AppBannerAd extends StatefulWidget {
   const AppBannerAd({Key? key}) : super(key: key);
+
+  @override
+  State<AppBannerAd> createState() => _AppBannerAdState();
+}
+
+class _AppBannerAdState extends State<AppBannerAd> {
+  late Future<bool> _isBannerBlockedFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _isBannerBlockedFuture = AdService.isBannerBlocked();
+  }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<bool>(
-      future: AdService.isBannerBlocked(),
+      future: _isBannerBlockedFuture,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return SizedBox.shrink();
